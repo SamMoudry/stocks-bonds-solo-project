91e7266c03552c8ed36cd180e5a2e1a8013147ab
SamMoudry <sammoudry@gmail.com>
2021-05-26 11:35:44 -0500

    newgame saga

---
+++ src/redux/sagas/newgame.saga.js
@@ -0,0 +1,16 @@
+import { takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* newGame(action) {
+    try{
+        yield axios.post('/api/newgame', action.payload);
+    }catch (error) {
+        console.log(error);
+    }
+}
+
+function* newGameSaga() {
+    yield takeLatest('NEW_GAME', newGame)
+}
+
+export default newGameSaga;
