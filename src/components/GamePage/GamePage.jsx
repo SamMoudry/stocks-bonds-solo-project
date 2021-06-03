311cf18d952982ff22ccbc8292734ab0ec2e5634
SamMoudry <sammoudry@gmail.com>
2021-06-03 10:41:57 -0500

    messing with rendering and hooks

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import './GamePage.css'
+import GameTable from './GameTable/GameTable.jsx';
 
 function GamePage() {
   const [value1, setValue1] = useState(100);
@@ -30,6 +31,7 @@ function GamePage() {
   const user_id = useSelector(store => store.user.id)
   const gameId = useSelector(store => store.setNewGame.gameId);
   const newYearData = useSelector(store => store.setNewYear);
+  let currentYear = newYearData[newYearData.length - 1];
   useEffect(() => {
     dispatch({ type: 'NEW_GAME', payload: {userId: user_id}});
   }, [dispatch, user_id]);
@@ -46,7 +48,7 @@ function GamePage() {
           );
       } else {
         return (
-          <button onClick={newYear}>Year {yearNum +1}</button>
+          <button onClick={newYear}>Year {yearNum}</button>
         );
       }
     }
@@ -71,126 +73,27 @@ function GamePage() {
       total_money: totalBalance, 
       stocks: stockDataToSend,
     }});
-    //dispatch({type: 'NEW_YEAR', payload: {game_id: gameId, year_number: yearNum}})
-    renderNewYear();
-  }
-
-<<<<<<< HEAD
-  const renderNewYear = () => {
-      // append column to the HTML table
-    let tbl = document.getElementById('stockboard'), // table reference
-        i;
-    // open loop for each row and append cell
-    for (i = 0; i < tbl.rows.length; i++) {
-      switch (i) {
-        case 0:
-      }
-      
-      
-      
-      
-      
-      // if (i == 0) {
-      //   createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), yearNum);
-      // } else {
-      //   createCell(tbl.rows[i].insertCell(tbl.rows[i].cells.length), i);
-      // } 
+    if (newYearData) {
+      setValues();
     }
-
   }
 
-    // create DIV element and append to the table cell
-  function createCell(cell, text, style) {
-    let div = document.createElement('div'), // create DIV element
-        txt = document.createTextNode(text); // create text node
-    div.appendChild(txt);                    // append text node to the DIV
-    div.setAttribute('class', style);        // set DIV class attribute
-    div.setAttribute('className', style);    // set DIV class attribute for IE (?!)
-    cell.appendChild(div);                   // append DIV to the table cell
+  const setValues = () => {
+    setValue2(currentYear.value[0]);
+    setValue3(currentYear.value[1]);
+    setValue4(currentYear.value[2]);
+    setValue5(currentYear.value[3]);
+    setValue6(currentYear.value[4]);
+    setValue7(currentYear.value[5]);
+    setValue8(currentYear.value[6]);
+    setValue9(currentYear.value[7]);
+    setValue10(currentYear.value[8]);
   }
-  
-=======
->>>>>>> e48ee13c79bc53eb00c0aeb7858687e8bfbea9c8
+
 
   return (
     <div className="container">
-      <table id="stockboard">
-        <tbody>
-        <tr>
-          <th className="stockheader">Stocks</th>
-        </tr>
-        <tr>
-          <td className="stockname">
-            <span id="stockname_1">Central City Bonds</span><br />
-            <div className="yieldstyle">YIELD 5%</div>
-            <input type="hidden" value={value1} id="value_0_1" />
-          </td>
-        </tr>
-        <tr>
-          <td className="stockname" >
-            <span id="stockname_2">Growth Corp</span><br />
-            <div className="yieldstyle">YIELD 1%</div>
-            <input type="hidden" value={value2} id="value_0_2" />
-          </td>
-        </tr>
-        <tr>
-          <td className="stockname">
-            <span id="stockname_3">Metro Prop</span><br />
-            <div className="yieldstyle">NO YIELD</div>
-            <input type="hidden" value={value3} id="value_0_3" />
-          </td>
-        </tr>
-        <tr>
-          <td className="stockname" >
-            <span id="stockname_4">Pioneer Mutl</span><br />
-            <div className="yieldstyle">YIELD 4%</div>
-            <input type="hidden" value={value4} id="value_0_4" />
-          </td>
-        </tr>
-        <tr>
-          <td className="stockname">
-            <span id="stockname_5">Shady Brooks</span><br />
-            <div className="yieldstyle">YIELD 7%</div>
-            <input type="hidden" value={value5} id="value_0_5" />
-          </td>
-        </tr>
-        <tr>
-          <td className="stockname" >
-            <span id="stockname_6">Stryker Drlg</span><br />
-            <div className="yieldstyle">NO YIELD</div>
-            <input type="hidden" value={value6} id="value_0_6" />
-          </td>
-        </tr>
-        <tr>
-          <td className="stockname">
-            <span id="stockname_7">Tri City Trans</span><br />
-            <div className="yieldstyle">NO YIELD</div>
-            <input type="hidden" value={value7} id="value_0_7" />
-          </td>
-        </tr>
-        <tr>
-          <td className="stockname" >
-            <span id="stockname_8">United Auto</span><br />
-            <div className="yieldstyle">YIELD 2%</div>
-            <input type="hidden" value={value8} id="value_0_8" />
-          </td>
-        </tr>
-        <tr>
-          <td className="stockname">
-            <span id="stockname_9">Uranium Ent</span><br />
-            <div className="yieldstyle">YIELD 6%</div>
-            <input type="hidden" value={value9} id="value_0_9" />
-          </td>
-        </tr>
-        <tr>
-          <td className="stockname" >
-            <span id="stockname_10">Valley Power</span><br />
-            <div className="yieldstyle">YIELD 3%</div>
-            <input type="hidden" value={value10} id="value_0_10" />
-          </td>
-        </tr>
-        </tbody>
-      </table><br />
+      <GameTable />
       {YearButton()}
       <div>Total Balance: {totalBalance}</div><br />
       <div>Yearly Earnings: {yearlyEarnings}</div>
